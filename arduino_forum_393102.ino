71f98f5411b531a3973fd1b43f2ed865f46a26c2
Paul Murray <pmurray@bigpond.com>
2016-04-12 19:29:39 +1000

    ok, now to write some kewl effects

--- arduino_forum_393102.ino
+++ arduino_forum_393102.ino
@@ -1,3 +1,9 @@
+/*
+   Copyright (c) Paul Murray, 2016. Released into the public domain
+   under the unlicense http://unlicense.org .
+*/
+
+
 #include <Adafruit_NeoPixel.h>
 
 #include "DebounceInput.h"
@@ -6,72 +12,164 @@
 
 class Button : DebouncedInput {
 
-  const unsigned long LONG_CLICK_ms = 1000;
-  const unsigned long DCLICK_ms = 250;
-   
-  unsigned long down;
-  int clickcount;
-  boolean clickfired;
+    const unsigned long LONG_CLICK_ms = 1000;
+    const unsigned long DCLICK_ms = 250;
 
-public:
-  Button(int pin) : DebouncedInput(pin)  {}
+    unsigned long down;
+    int clickcount;
+    boolean clickfired;
 
-  void (*click_callback)(int);
-  void (*longclick_callback)(int);
+  public:
+    Button(int pin) : DebouncedInput(pin)  {}
 
-  void loop() {
-    read();
+    virtual void onClick(int);
+    virtual void onLongClick(int);
 
-    if(falling()) {
-      if(millis() - down > DCLICK_ms) clickcount = 0; 
-      clickcount ++;
-      clickfired = false;
-      down = millis();
-    }
-    else
-    if(rising()) {
-      if(!clickfired) {
-        if(click_callback) click_callback(clickcount);
+    void loop() {
+      read();
+
+      if (falling()) {
+        if (millis() - down > DCLICK_ms) clickcount = 0;
+        clickcount ++;
+        clickfired = false;
+        down = millis();
+      }
+      else if (rising()) {
+        if (!clickfired) {
+          onClick(clickcount);
+          clickfired = true;
+        }
+      }
+      else if (low() && !clickfired && millis() - down > LONG_CLICK_ms) {
+        onLongClick(clickcount);
         clickfired = true;
       }
     }
-    else if(low() && !clickfired && millis() - down > LONG_CLICK_ms) {
-      if(longclick_callback) longclick_callback(clickcount);
-      clickfired = true;
-    }
-  }
+};
+
+class StrandController;
+
+class Effect
+{
+  protected:
+    Effect();
+  public:
+    virtual void start(StrandController *controller);
+    virtual void loop(StrandController *controller);
+    virtual void stop(StrandController *controller);
 };
 
 
-Button b1(4), b2(5), b3(6);
 
-void click(int i) {
-      Serial.println("click ");
-      Serial.print(i);
-      Serial.println();
+class StrandController {
+    static int nEffects;
+    static Effect *effects[20]; // this needs to be big enbough for the effect singletons
+
+    Adafruit_NeoPixel strand;
+    int currentEffect = 0;
+
+  public:
+
+    static void register_effect(Effect *e) {
+      effects[nEffects++] = e;
+    }
+
+
+    // these variables are a scratchpad area for the various effects. HOw they are used
+    // is defined by the effect. prev_ms and ms are maintained by the loop method here, but are
+    // available to the effects
+
+    unsigned long prev_ms, ms;
+    unsigned long time[10];
+    unsigned n[10];
+    float f[10];
+    boolean isOn = false;
+
+    StrandController(int strandSize, int strandPin) :
+      strand(strandSize, strandPin, NEO_GRB + NEO_KHZ800) {
+    }
+
+    void loop() {
+      if (!isOn) return;
+      ms = millis();
+      effects[currentEffect]->loop(this);
+      prev_ms = ms;
+    }
+
+    void off() {
+      if (!isOn) return;
+      ms = prev_ms = millis();
+      effects[currentEffect]->stop(this);
+      isOn = false;
+    }
+
+    void on() {
+      if (isOn) return;
+      ms = prev_ms = millis();
+      effects[currentEffect]->start(this);
+      isOn = true;
+    }
+
+    void next() {
+      if (isOn) {
+        effects[currentEffect]->stop(this);
+      }
+      if (++currentEffect >= nEffects) {
+        currentEffect = 0;
+      }
+      if (isOn) {
+        effects[currentEffect]->start(this);
+      }
+      prev_ms = ms = millis();
+    }
+
+};
+
+Effect::Effect() {
+  StrandController::register_effect(this);
 }
 
-void longclick(int i) {
-      Serial.println("longclick ");
-      Serial.print(i);
-      Serial.println();
-}
+class StrandControllerButton: Button {
+    StrandController &controller;
+  public :
+    StrandControllerButton(StrandController &controller, int pin) :
+      Button(pin), controller(controller) {
+    }
+
+    void loop() {
+      Button::loop();
+    }
+
+    void onClick(int) {
+      controller.next();
+    }
+
+    void onLongClick(int) {
+      if (controller.isOn) {
+        controller.off();
+      }
+      else {
+        controller.on();
+      }
+    }
+
+
+};
+
+
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(57600);
-
-  b1.click_callback = click;
-  b1.longclick_callback = longclick;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  b1.loop();
-  b2.loop();
-  b3.loop();
 }
 
 
