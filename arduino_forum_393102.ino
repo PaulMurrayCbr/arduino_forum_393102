9536b3020355193b421d3f3a0ce2931481bf47bb
Paul Murray <pmurray@bigpond.com>
2016-04-12 20:57:46 +1000

    Well, that seems to work.

--- arduino_forum_393102.ino
+++ arduino_forum_393102.ino
@@ -12,7 +12,7 @@
 
 class Button : DebouncedInput {
 
-    const unsigned long LONG_CLICK_ms = 1000;
+    const unsigned long LONG_CLICK_ms = 500;
     const unsigned long DCLICK_ms = 250;
 
     unsigned long down;
@@ -22,8 +22,8 @@ class Button : DebouncedInput {
   public:
     Button(int pin) : DebouncedInput(pin)  {}
 
-    virtual void onClick(int);
-    virtual void onLongClick(int);
+    virtual void onClick(int) {}
+    virtual void onLongClick(int) {}
 
     void loop() {
       read();
@@ -48,32 +48,49 @@ class Button : DebouncedInput {
 };
 
 class StrandController;
+class Effect;
+
+
+int nEffects;
+Effect *effects[20]; // this needs to be big enbough for the effect singletons
+void register_effect(Effect *e) {
+  effects[nEffects++] = e;
+}
+
 
 class Effect
 {
-  protected:
-    Effect();
   public:
-    virtual void start(StrandController *controller);
-    virtual void loop(StrandController *controller);
-    virtual void stop(StrandController *controller);
+    Effect() {
+      register_effect(this);
+    }
+    virtual void start(StrandController *controller) {}
+    virtual void loop(StrandController *controller) {}
+    virtual void stop(StrandController *controller) {}
+
+
+    // Input a value 0 to 255 to get a color value.
+    // The colours are a transition r - g - b - back to r.
+    uint32_t wheel(Adafruit_NeoPixel &strip, byte WheelPos) {
+      WheelPos = 255 - WheelPos;
+      if (WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+      }
+      if (WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+      }
+      WheelPos -= 170;
+      return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    }
 };
 
 
-
 class StrandController {
-    static int nEffects;
-    static Effect *effects[20]; // this needs to be big enbough for the effect singletons
-
-    Adafruit_NeoPixel strand;
-    int currentEffect = 0;
 
   public:
-
-    static void register_effect(Effect *e) {
-      effects[nEffects++] = e;
-    }
-
+    Adafruit_NeoPixel strand;
+    int currentEffect = 0;
 
     // these variables are a scratchpad area for the various effects. HOw they are used
     // is defined by the effect. prev_ms and ms are maintained by the loop method here, but are
@@ -89,6 +106,10 @@ class StrandController {
       strand(strandSize, strandPin, NEO_GRB + NEO_KHZ800) {
     }
 
+    void setup() {
+      strand.begin();
+    }
+
     void loop() {
       if (!isOn) return;
       ms = millis();
@@ -101,6 +122,8 @@ class StrandController {
       ms = prev_ms = millis();
       effects[currentEffect]->stop(this);
       isOn = false;
+      strand.clear();
+      strand.show();
     }
 
     void on() {
@@ -117,6 +140,8 @@ class StrandController {
       if (++currentEffect >= nEffects) {
         currentEffect = 0;
       }
+      strand.clear();
+      strand.show();
       if (isOn) {
         effects[currentEffect]->start(this);
       }
@@ -124,10 +149,6 @@ class StrandController {
     }
 
 };
-
-Effect::Effect() {
-  StrandController::register_effect(this);
-}
 
 class StrandControllerButton: Button {
     StrandController &controller;
@@ -152,23 +173,82 @@ class StrandControllerButton: Button {
         controller.on();
       }
     }
-
-
 };
 
+class Point : Effect {
+  public:
+    void start(StrandController *controller) {}
+    void stop(StrandController *controller) {}
 
+    // an extremely simple effect that moves the led along by one every 10th of a second.
+    void loop(StrandController *controller) {
+      int a = (controller->prev_ms / 100) % controller->strand.numPixels();
+      int b = (controller->ms / 100) % controller->strand.numPixels();
 
+      if (a != b) {
+        controller->strand.setPixelColor(a, 0);
+        controller->strand.setPixelColor(b, controller->strand.Color(255, 255, 255));
+        controller->strand.show();
+      }
+    }
 
+} point;
 
+class Rainbow : Effect {
+  public:
+    void start(StrandController *controller) {}
+    void stop(StrandController *controller) {}
+
+    // an extremely simple effect that moves the led along by one every 10th of a second.
+    void loop(StrandController *controller) {
+      int t =  (controller->ms ) & 255;
+
+      for(int i = 0; i<controller->strand.numPixels(); i++) {
+        controller->strand.setPixelColor(i, wheel(controller->strand,  ( (i*256/controller->strand.numPixels())+t) & 255));
+      }
+
+      controller->strand.show();
+    }
+
+} rainbow;
+
+// atttach my four neopixel rings to pins 12-9. Two of my rings are 24-led, and two are 16 led
+
+StrandController s12(24, 12), s11(24, 11), s10(16, 10), s9(16, 9);
+
+// attach my three buttons. I will run strips 11 and 10 both off button 5.
+
+StrandControllerButton b12(s12, 4), b11(s11, 5), b10(s10, 5), b9(s9, 6);
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(57600);
+  while (!Serial);
+
+  Serial.print(" nEffects is ");
+  Serial.print(nEffects);
+  Serial.println();
+
+  s12.setup();
+  s11.setup();
+  s10.setup();
+  s9.setup();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  s12.loop();
+  s11.loop();
+  s10.loop();
+  s9.loop();
+
+  b12.loop();
+  b11.loop();
+  b10.loop();
+  b9.loop();
 
 }
 
